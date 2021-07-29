2a27d11b6692747b1e8c4290352ab9b1b813eadd
Justin Turner <jmturner638@gmail.com>
2021-07-29 06:18:27 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #write your code below this line
+    count = 0
+    sum_of_num  = 0
+
+    while True:
+        number = int(input("Give a number:"))
+
+        if number == 0:
+            break
+
+        count = count + 1
+        sum_of_num = sum_of_num + number
+    
+    average = sum_of_num / count
+
+    print("Average of the numbers: " + str(average))
 
 if __name__ == '__main__':
     main()
